d68fd2ac8934099292d38780dae19243d24014f7
SashaProgrammist <49712254+SashaProgrammist@users.noreply.github.com>
2023-07-04 00:34:09 +0300

    change App add saveAnimation

--- App.py
+++ App.py
@@ -3,6 +3,9 @@ import moderngl as mgl
 import moderngl_window as mglw
 from moderngl_window import geometry
 from moderngl_window.geometry.attributes import AttributeNames
+from PIL import Image
+import os
+import shutil
 
 from Ants import Ants
 from Pheromone import Pheromone
@@ -18,6 +21,33 @@ class App(mglw.WindowConfig):
     cursor = True
     aspect_ratio = None
     attributeNames = AttributeNames(texcoord_0="in_texCoord")
+    indexFrame = None
+
+    @classmethod
+    def saveAnimation(cls):
+        cls.indexFrame = 0
+
+        def render(self: cls, time, frame_time):
+            cls._render(self, time, frame_time)
+
+            if cls.indexFrame % 2:
+                Image.frombytes('RGB', self.ctx.fbo.size,
+                                self.ctx.fbo.read(), 'raw',
+                                'RGB', 0, -1).save(f"animation/{cls.indexFrame // 2}.png")
+            cls.indexFrame += 1
+
+        cls.render = render
+
+        folder = "animation"
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -55,7 +85,7 @@ class App(mglw.WindowConfig):
         self.ants.set_newResolution()
         self.pheromone.set_newResolution()
 
-    def _render(self):
+    def renderVao(self):
         self.mapp.render()
         self.global_quad.render(self.global_prog)
         # self.ants.render()
@@ -65,7 +95,7 @@ class App(mglw.WindowConfig):
         self.ants.update(time, frame_time)
         self.pheromone.update(time, frame_time)
 
-    def render(self, time, frame_time):
+    def _render(self, time, frame_time):
         if frame_time > 0.03:
             frame_time = 0
 
@@ -73,7 +103,10 @@ class App(mglw.WindowConfig):
 
         self.update(time, frame_time)
 
-        self._render()
+        self.renderVao()
+
+    def render(self, time, frame_time):
+        self._render(time, frame_time)
 
     @staticmethod
     def set_uniform(prog, name, value):
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from App import *
 
 if __name__ == '__main__':
+    # App.saveAnimation()
     mglw.run_window_config(App)
